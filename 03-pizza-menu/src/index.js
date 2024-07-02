6157f9ad0a548ec50c79cfa365025d85e0f65c3f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-02 15:59:23 +0200

    "Added paragraph of text above pizza list in Menu component"

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -78,12 +78,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p> We're still workibg on our menu. Please come back later...</p>
       )}
