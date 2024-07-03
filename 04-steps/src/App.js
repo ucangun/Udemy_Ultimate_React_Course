5e804b8562bcbc38faa770866f81a8ce32c4cc3c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-03 22:03:36 +0200

    "Added handlePrevious and handleNext functions and attached them to Previous and Next buttons"

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -6,6 +6,15 @@ const messages = [
 
 function App() {
   const step = 1;
+
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +26,16 @@ function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
