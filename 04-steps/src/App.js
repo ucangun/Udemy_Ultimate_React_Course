fe5644a988fcb7ba36a6841bb941668eeb34f92e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-03 22:29:20 +0200

    "Added useState hooks for step and test variables, updated handlePrevious and handleNext functions, and modified step indicators and message display in App component."

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,25 +7,31 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "Jonas" });
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
+
+    //  BAD PRACTICE
+    // test.name = "Fred";
+    // setTest({ name: "Umut" });
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
+        {/* {test.name} */}
       </p>
       <div className="buttons">
         <button
