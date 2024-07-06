a3c61e92bb2908f1150c19c0fc9bd6a2270358dc
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-06 15:21:31 +0200

    "Removed App.css file, added initialItems array and implemented PackingList and Item components in App.js"

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,4 +1,8 @@
-import "./App.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
 
 function App() {
   return (
@@ -22,11 +26,31 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list"> LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have X items on your list, and you already packed X</em>
     </footer>
   );
