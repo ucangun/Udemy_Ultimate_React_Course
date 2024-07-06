a1bafcf7b7bf1e8ed1c0db9f38c43efe6fbfc3d3
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-06 16:06:55 +0200

    "Added state and form handling to App.js, updated initialItems array, and modified Form and PackingList components"

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 function App() {
@@ -19,18 +21,53 @@ function Logo() {
   return <h1> 🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
-      <h3>What do you need your 😍 trip ?</h3>{" "}
-    </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need your 😍 trip ?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
+
 function PackingList() {
   return (
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
