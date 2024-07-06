5361ecffc37247b5b52402127861095c4a867c3a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-06 15:09:51 +0200

    "Added components for logo, form, packing list, and stats to App.js"

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> 🏝️ Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need your 😍 trip ?</h3>{" "}
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list"> LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already packed X</em>
+    </footer>
+  );
 }
 
 export default App;
