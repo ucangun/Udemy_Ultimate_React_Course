e060594c5a5ee03080bd23fda5ca547c46d0304e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-10 16:49:54 +0200

    "Updated App.js to add item handling, modified Form and PackingList components, and changed index.css styling"

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +26,8 @@ function App() {
 function Logo() {
   return <h1> 🏝️ Far Away 🧳</h1>;
 }
-function Form() {
+
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState();
 
@@ -36,6 +43,8 @@ function Form() {
       id: Date.now(),
     };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -62,11 +71,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
