59272fdd5cf88e799ca5b9f8369d5e2e3b7663d8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-15 00:26:16 +0200

    "Added FormAddFriend component and Button component, and updated App.js to include FormAddFriend and Button elements"

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -26,6 +26,8 @@ const App = () => {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
     </div>
   );
@@ -59,8 +61,26 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend name</label>
+      <input type="text" />
+
+      <label>🌄 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
   );
 }
 
