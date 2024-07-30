7c48cdd69f46dfa7a1b6a31a21400f16e6147941
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-30 12:19:06 +0200

    "Removed useState and useEffect from App component, introduced CitiesProvider and useCities hook, and updated CityList and CountryList components to use the new hook."

--- 11-worldwise/src/components/CityList.jsx
+++ 11-worldwise/src/components/CityList.jsx
@@ -1,9 +1,12 @@
+import { useCities } from "../contexts/CitiesContext";
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
 
-const CityList = ({ cities, isLoading }) => {
+const CityList = () => {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
