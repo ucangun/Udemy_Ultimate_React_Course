eb811b5401ba9a13fecec5d07cea5f8b6aefa24c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-30 16:44:34 +0200

    "Updated Map component to use cities from context, changed zoom level, and replaced static marker with dynamic markers for each city"

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -2,9 +2,13 @@ import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 const Map = () => {
   const navigate = useNavigate();
+
+  const { cities } = useCities();
+
   const [mapPosition, setMapPosition] = useState([41, 29]);
 
   const [searchParam, setSearchParam] = useSearchParams();
@@ -20,7 +24,7 @@ const Map = () => {
     >
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -28,11 +32,16 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
