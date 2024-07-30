848607b00fd3320eff885a549b46e56d2e2c3456
ucangun <149247682+ucangun@users.noreply.github.com>
2024-07-30 15:51:43 +0200

    "Added Spinner component to City component and updated CityItem component to highlight active city"

--- 11-worldwise/src/components/City.jsx
+++ 11-worldwise/src/components/City.jsx
@@ -2,6 +2,7 @@ import styles from "./City.module.css";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -13,7 +14,7 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const { getCity, currentCity } = useCities();
+  const { getCity, currentCity, isLoading } = useCities();
 
   useEffect(
     function () {
@@ -21,8 +22,10 @@ function City() {
     },
     [id]
   );
-  console.log(currentCity);
+
   const { cityName, emoji, date, notes } = currentCity;
+
+  if (isLoading) return <Spinner />;
 
   return (
     <div className={styles.city}>
