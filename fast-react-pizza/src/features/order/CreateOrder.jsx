e551c2c30769f3947c6b0ca5921c6a1464b30b4d
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-18 19:10:07 +0200

    "Updated styles and layout for Cart, CreateOrder, and AppLayout components"

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -79,7 +79,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button disabled={isSubmitting} className="inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed " >
             {isSubmitting ? "Placing order...." : "Order now"}
           </button>
         </div>
