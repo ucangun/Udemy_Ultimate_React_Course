d29fc98bbb6df425f58d6e5ee34fa38d5b3f053f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-18 10:12:24 +0200

    "Updated ESLint config, added react-router-dom, and created routes for App component"

--- fast-react-pizza/.eslintrc.cjs
+++ fast-react-pizza/.eslintrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
@@ -7,12 +6,12 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    "no-unused-vars": "warn",
+    "no-unused-vars": "off",
+    "react/prop-types": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
