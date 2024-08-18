01a5cfe4a75a749dded70a424a3db3025ab44db8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-18 10:27:56 +0200

    "Refactor App routing and update CartOverview link to use react-router-dom"

---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Umut</p>
+    </header>
+  );
+};
+
+export default Header;
