6aa3ee085fe0f7709be5a99ee49b07a9e0592b4e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-18 17:52:15 +0200

    "Updated HTML and CSS in various components, including index.html, CartOverview, Header, and Home, with changes to class names, element structures, and added components."

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
-const Header = () => {
+function Header() {
   return (
-    <header className="bg-yellow-500" >
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between px-4 py-3 uppercase bg-yellow-400 border-b border-stone-200 sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-
-      <p>Umut</p>
+      <Username />
     </header>
   );
-};
+}
 
 export default Header;
