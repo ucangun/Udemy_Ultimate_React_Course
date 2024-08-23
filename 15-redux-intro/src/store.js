fafa266434ba087f97a93800d07413488ddabe2c
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-23 14:27:38 +0200

    "Added Redux dependency and imported store in index.js; removed App.css file"

---
+++ 15-redux-intro/src/store.js
@@ -0,0 +1,64 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      // LATER
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: "account/deposit",
+  payload: 500,
+});
+
+store.dispatch({
+  type: "account/withdraw",
+  payload: 200,
+});
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: {
+    amount: 1000,
+    purpose: "Car loan",
+  },
+});
+
+store.dispatch({
+  type: "account/payLoan",
+});
+
+console.log(store.getState());
