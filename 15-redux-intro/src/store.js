a80cfe9988a2417a1b2025ec97b625ddb9509707
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-24 03:44:05 +0200

    "Added Redux DevTools extension, updated customerSlice.js and store.js, and modified package.json and package-lock.json files"

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
