8e9a58a24b5eb57192eaa0efa2871d18c23d34ba
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-26 15:14:41 +0200

    delete unneccessary folder and create cartSlice and update store.js

--- 16-fast-react-pizza/src/store.js
+++ 16-fast-react-pizza/src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./features/user/userSlice"
+import userReducer from "./features/user/userSlice";
+import cartReducer from "./features/cart/cartSlice";
 
 
 
@@ -7,6 +8,7 @@ const store = configureStore(
   {
     reducer: {
       user: userReducer,
+      cart: cartReducer,
     } 
   }
 )
