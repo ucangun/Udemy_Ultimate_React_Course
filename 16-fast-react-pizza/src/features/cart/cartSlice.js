e3686df26c6cb4af2be230729600ac8e24ae3cc8
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-26 15:46:11 +0200

    "Updated CartOverview component to use useSelector and added getTotalCartPrice and getTotalCartQuantity selectors to cartSlice"

--- 16-fast-react-pizza/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/src/features/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  // cart: [],
+   cart: [],
 
+  /*
  cart: [
    {
      pizzaId: 12,
@@ -12,6 +13,7 @@ const initialState = {
        totalPrice: 32,
     },
    ],
+   */
 };
 
 const cartSlice = createSlice({
@@ -58,3 +60,21 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+
+
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
+
+
+// 'reselect'
