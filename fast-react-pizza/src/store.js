2462607ac52b114cefa3fc6bb16b4b4ceea9ef59
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-26 14:31:03 +0200

    "Added Redux toolkit and react-redux dependencies, implemented userSlice with updateName reducer, and wrapped App with Provider in main.jsx"

---
+++ fast-react-pizza/src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./features/user/userSlice"
+
+
+
+const store = configureStore(
+  {
+    reducer: {
+      user: userReducer,
+    } 
+  }
+)
+
+export default store 
