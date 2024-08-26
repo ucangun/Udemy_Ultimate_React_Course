943ab9d9b5c6c053f9a60d1b4c89af9f98cf371a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-26 16:20:09 +0200

    "Refactor CartItem and MenuItem components to use new DeleteItem component and update cart item handling"

--- 16-fast-react-pizza/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,11 +1,15 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { addItem } from '../cart/cartSlice';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
 
   const handleAddToCart = () => {
     const newItem = {
@@ -40,7 +44,14 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut &&  (
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+    
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
+
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
