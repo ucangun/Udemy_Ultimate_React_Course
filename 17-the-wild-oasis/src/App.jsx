abb7c07d500f199ef5fd322fb27c365e48f0787e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-28 10:36:52 +0200

    "Updated App.jsx to import Heading component and use it in place of h1 tags;"

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyle";
+import Heading from "./ui/Heading";
 
 styled.h1``;
 
 const App = () => {
   return (
     <>
+      <Heading as="h1">ASD</Heading>
+      <Heading as="h3">ASD</Heading>
+
       <GlobalStyles />
-      <h1>Hello World</h1>
     </>
   );
 };
