0f281e3856ab84f76ffc429ba8037e5faba1adf3
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-28 15:29:26 +0200

    "Added @supabase/supabase-js dependency and updated package-lock.json"

---
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://jnjfsxfuyfgipjcqzdkr.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuamZzeGZ1eWZnaXBqY3F6ZGtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4MzkwNTUsImV4cCI6MjA0MDQxNTA1NX0.sp-6VmsKRdM_QHvQQ1o5FA4KwV-CxXObUjWb-3he-jU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
