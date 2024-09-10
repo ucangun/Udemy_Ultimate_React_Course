8d5ff0834bcd0319ace0a6b20ecfb735d4e1dc7f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-09-10 22:37:20 +0200

    "Refactor CabinRow, CabinTable, and related UI components to use new Table and Menus components"

--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,7 +1,7 @@
-import AddCabin from "../features/cabins/AddCabin";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
   return (
